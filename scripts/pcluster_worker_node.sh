3f42efbb75857749c16fd0b8b3859d2a223a57b5
Ben Eisenbraun <bene@hkl.hms.harvard.edu>
2023-05-26 11:38:20 -0400

    Cleanup worker node config

--- scripts/pcluster_worker_node.sh
+++ scripts/pcluster_worker_node.sh
@@ -1,34 +1,24 @@
+#!/bin/bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-#!/bin/bash
 
 yum -y -q install jq mysql amazon-efs-utils
 # Get OOD Stack data
 OOD_STACK_NAME=$1
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/region)
 
-OOD_STACK=$(aws cloudformation describe-stacks --stack-name $OOD_STACK_NAME --region $REGION )
+OOD_STACK=$(aws cloudformation describe-stacks --stack-name "$OOD_STACK_NAME" --region "$REGION" )
 
-
-S3_CONFIG_BUCKET=$(echo $OOD_STACK | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="ClusterConfigBucket") | .OutputValue')
-EFS_ID=$(echo $OOD_STACK | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="EFSMountId") | .OutputValue')
+S3_CONFIG_BUCKET=$(echo "$OOD_STACK" | jq -r '.Stacks[].Outputs[] | select(.OutputKey=="ClusterConfigBucket") | .OutputValue')
 
 # Copy Common Munge Key
-aws s3 cp s3://$S3_CONFIG_BUCKET/munge.key /etc/munge/munge.key
+aws s3 cp "s3://$S3_CONFIG_BUCKET/munge.key" /etc/munge/munge.key
 chown munge: /etc/munge/munge.key
 chmod 400 /etc/munge/munge.key
 systemctl restart munge
 
-# Add entry for fstab so mounts on restart
-#mkdir /shared
-#echo "$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EFS_ID}.efs.$REGION.amazonaws.com:/ /shared efs _netdev,noresvport,tls,iam 0 0" >> /etc/fstab
-mount -a
-
 # Add spack-users group
 groupadd spack-users -g 4000
-
-# change to using ad user login, this is no longer needed 
-#/shared/copy_users.sh
 
 #fix the sssd script so getent passws command can find the domain user
 #This line allows the users to login without the domain name
